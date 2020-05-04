1fe948d40a5610ace39fab4bb66c8d3e6fba3aac
1029574707@qq.com <1029574707@qq.com>
2020-05-04 09:44:03 +0800

    实习计划，日记，报告增加对评价的绑定

--- src/main/java/com/zshnb/ballplatform/service/EvaluationServiceDiy.java
+++ src/main/java/com/zshnb/ballplatform/service/EvaluationServiceDiy.java
@@ -1,7 +1,13 @@
 package com.zshnb.ballplatform.service;
 
 import com.zshnb.ballplatform.entity.Evaluation;
+import com.zshnb.ballplatform.entity.PracticeDiary;
+import com.zshnb.ballplatform.entity.PracticePlan;
+import com.zshnb.ballplatform.entity.PracticeReport;
 import com.zshnb.ballplatform.mapper.EvaluationDao;
+import com.zshnb.ballplatform.mapper.PracticeDiaryDao;
+import com.zshnb.ballplatform.mapper.PracticePlanDao;
+import com.zshnb.ballplatform.mapper.PracticeReportDao;
 import com.zshnb.ballplatform.service.inter.MPEvaluationService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zshnb.ballplatform.utils.DateUtils;
@@ -22,12 +28,40 @@ import java.util.Date;
 public class EvaluationServiceDiy extends ServiceImpl<EvaluationDao, Evaluation> implements MPEvaluationService {
 
     @Autowired
-    EvaluationDao evaluationDao;
+    private EvaluationDao evaluationDao;
+
+    @Autowired
+    private PracticePlanDao planDao;
+
+    @Autowired
+    private PracticeDiaryDao diaryDao;
+
+    @Autowired
+    private PracticeReportDao reportDao;
 
     @Override
     public void add(Evaluation evaluation) {
         evaluation.setCreateTime(DateUtils.format(new Date()));
         evaluationDao.insert(evaluation);
+
+        int type = evaluation.getType();
+        switch (type) {
+            case 1:
+                PracticePlan plan = planDao.selectById(evaluation.getAssociateId());
+                plan.setEvaluationId(evaluation.getId());
+                planDao.updateById(plan);
+                break;
+            case 2:
+                PracticeDiary diary = diaryDao.selectById(evaluation.getAssociateId());
+                diary.setEvaluationId(evaluation.getId());
+                diaryDao.updateById(diary);
+                break;
+            case 3:
+                PracticeReport report = reportDao.selectById(evaluation.getAssociateId());
+                report.setEvaluationId(evaluation.getId());
+                reportDao.updateById(report);
+                break;
+        }
     }
 
     @Override
