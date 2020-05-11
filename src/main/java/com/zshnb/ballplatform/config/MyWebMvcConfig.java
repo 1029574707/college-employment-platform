e6dfaf765d640d64733bbd147c4eb1d6952bfc20
1029574707@qq.com <1029574707@qq.com>
2020-05-11 22:11:35 +0800

    修改上传图片设置，前端可以直接访问静态资源

--- src/main/java/com/zshnb/ballplatform/config/MyWebMvcConfig.java
+++ src/main/java/com/zshnb/ballplatform/config/MyWebMvcConfig.java
@@ -1,19 +1,24 @@
-package com.zshnb.ballplatform.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * CreateDate：2020/5/6 <br/>
- * Author：WangHao <br/>
- * Description:
- **/
-@Configuration
-public class MyWebMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/**").addResourceLocations("./upload-img/");
-    }
-}
+// package com.zshnb.ballplatform.config;
+//
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+// /**
+//  * CreateDate：2020/5/6 <br/>
+//  * Author：WangHao <br/>
+//  * Description:
+//  **/
+// @Configuration
+// public class MyWebMvcConfig implements WebMvcConfigurer {
+//
+//     @Value("${spring.file.upload-dir}")
+//     private String fileUploadDir;
+//
+//     @Override
+//     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//         registry.addResourceHandler("/upload/**").addResourceLocations(fileUploadDir);
+//         System.out.println();
+//     }
+// }
--- src/main/java/com/zshnb/ballplatform/utils/ImageUtils.java
+++ src/main/java/com/zshnb/ballplatform/utils/ImageUtils.java
@@ -35,6 +35,6 @@ public class ImageUtils {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "upload/" + fileName;
+        return fileName;
     }
 }
